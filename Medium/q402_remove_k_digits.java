792759759aa993118e10eea23d2180062420f046
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2020-06-19 20:50:21 -0400

    402, 413

---
+++ Medium/q402_remove_k_digits.java
@@ -0,0 +1,51 @@
+/** Given a non-negative integer num represented as a string, remove k digits from the number
+  * so that the new number is the smallest possible.
+  *
+  * Note:
+  * The length of num is less than 10002 and will be ≥ k.
+  * The given num does not contain any leading zero.
+  *
+  * Example 1:
+  * Input: num = "1432219", k = 3
+  * Output: "1219"
+  * Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is
+  * the smallest.
+  *
+  * Example 2:
+  * Input: num = "10200", k = 1
+  * Output: "200"
+  * Explanation: Remove the leading 1 and the number is 200. Note that the output must not
+  *   contain leading zeroes.
+  *
+  * Example 3:
+  * Input: num = "10", k = 2
+  * Output: "0"
+  * Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+  */
+
+class Solution {
+    // keep removing the first digit which is greater than its subsequent digit.
+    public String removeKdigits(String num, int k) {
+        if (k == num.length()) return "0";
+
+        StringBuilder sb = new StringBuilder(num);
+        sb.append('0');
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < sb.length() - 1; j++) {
+                if (sb.charAt(j) > sb.charAt(j+1)) {
+                    sb.deleteCharAt(j);
+                    break;
+                }
+            }
+        }
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        if (sb.length() == 0) {
+            return "0";
+        } else {
+            sb.deleteCharAt(sb.length() - 1);
+            return sb.toString();
+        }
+    }
+}
