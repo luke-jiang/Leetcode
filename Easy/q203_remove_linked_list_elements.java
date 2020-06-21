9ea7bc2c3f5e04a97f4ff444401e79866814bd54
Luke Jiang <39086121+luke-jiang@users.noreply.github.com>
2020-06-21 01:30:35 -0400

    200, 203,204

---
+++ Easy/q203_remove_linked_list_elements.java
@@ -0,0 +1,27 @@
+/** Remove all elements from a linked list of integers that have value val.
+  * Example:
+  * Input:  1->2->6->3->4->5->6, val = 6
+  * Output: 1->2->3->4->5
+  */
+  
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return head;
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while (prev != null && curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+                curr.next = null;
+            } else {
+                prev = prev.next;
+            }
+            if (prev != null) curr = prev.next;
+
+        }
+
+        return head.val == val ? head.next : head;
+    }
+}
